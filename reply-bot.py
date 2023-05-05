19c6a03a25998dc7c2c383faabcbb1d54b817a8e
Abdellah El Azraoui <ael-azra@student.1337.ma>
2023-05-05 16:23:52 +0100

    working on reply bot

---
+++ reply-bot.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import tweepy
+import os
+
+load_dotenv()
+api_key = os.environ["API_KEY"]
+api_secret = os.environ["API_SECRET"]
+bearer_token = os.environ["BEARER_TOKEN"]
+access_token = os.environ["ACCESS_TOKEN"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
+
+client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
+auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
+
